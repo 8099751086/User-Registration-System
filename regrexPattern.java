0e3c0d680a97d076ff4dae6dd794625fab8585ad
8099751086 <dilwarhussainit24@gmail.com>
2022-08-18 00:29:46 +0530

    Regex Pattern

---
+++ regrexPattern.java
@@ -0,0 +1,22 @@
+package com.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class regrexPattern {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First name : ");
+		String firstName = sc.nextLine();
+
+		boolean validatefirstName = Pattern.matches("^[A-Z][a-z]{3}$", firstName);
+
+		if (validatefirstName) {
+			System.out.println("firstname is correct ");
+		} else {
+			System.out.println("firstname is incorrect ");
+		}
+	}
+
+}
